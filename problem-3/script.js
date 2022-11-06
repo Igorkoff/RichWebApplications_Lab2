e06365e21b8fc7ddfb78eb33484be343f026e72d
Igor Alekhnovych <igoraleh@gmail.com>
2022-11-06 14:55:17 +0000

    Problem 3: Styling Adjustments

--- problem-3/script.js
+++ problem-3/script.js
@@ -1,5 +1,6 @@
 const mainElement = document.querySelector('.main');
 const wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
 
 // create form, input and search button
 
@@ -8,12 +9,15 @@ formElement.classList.add('search');
 formElement.addEventListener('submit', async (e) => {
     e.preventDefault();
     const inputValue = Object.fromEntries(new FormData(e.target));
-    const response = await fetch(`https://api.github.com/users/${inputValue.name}`);
+    const userResponse = await fetch(`https://api.github.com/users/${inputValue.name}`);
 
-    if (response.ok) {
-        const data = await response.json();
-        wrapper.appendChild(createProfileElement(data));
+    if (userResponse.ok) {
+        const reposResponse = await fetch(`https://api.github.com/users/${inputValue.name}/repos`);
+        const reposData = await reposResponse.json();
+        const userData = await userResponse.json();
+        wrapper.appendChild(createProfileElement(userData, reposData));
         mainElement.appendChild(wrapper);
+        inputElement.value = '';
     } else {
         alert("User Not Found");
     }
@@ -37,20 +41,53 @@ formElement.appendChild(searchButtonElement);
 
 mainElement.appendChild(formElement);
 
-// function to create profile card
+// function to create profile information
 
-function createProfileElement(profileData) {
+function createProfileElement(profileData, reposData) {
+
+    // create a div for information and repos cards
+
     const element = document.createElement('div');
-    element.classList.add('profile');
-    element.innerHTML = `
-    <img class="search-image" src=${profileData.avatar_url}></img>
-    <p class="search-text"><span>Name: </span>${profileData.name}</p>
-    <p class="search-text"><span>Username: </span>${profileData.login}</p>
-    <p class="search-text"><span>Email Address: </span>${profileData.email}</p>
-    <p class="search-text"><span>Location: </span>${profileData.location}</p>
-    <p class="search-text"><span>Number of Gists: </span>${profileData.public_gists}</p>
+    element.classList.add('record');
+
+    // create user information card
+
+    const profile = document.createElement('div');
+    profile.classList.add('profile');
+    profile.innerHTML = `
+    <div class="user-info">
+        <img class="search-image" src=${profileData.avatar_url}></img>
+        <p class="search-text"><span>Name: </span>${profileData.name}</p>
+        <p class="search-text"><span>Username: </span>${profileData.login}</p>
+        <p class="search-text"><span>Email Address: </span>${profileData.email}</p>
+        <p class="search-text"><span>Location: </span>${profileData.location}</p>
+        <p class="search-text"><span>Number of Gists: </span>${profileData.public_gists}</p>
+    </div>
     `
-    element.appendChild(createDeleteButtonElement())
+    profile.appendChild(createDeleteButtonElement());
+
+    // create repos card
+
+    const repos = document.createElement('div');
+    repos.classList.add('repos');
+
+    reposData.forEach((repo) => {
+        const element = document.createElement('div');
+        element.innerHTML = `
+        <div class="repo">
+            <a class="search-text" href=${repo.html_url}>${repo.name}</a>
+            <p class="search-text">${repo.description}</p>
+            <hr>
+        </div>
+        `
+        repos.appendChild(element);
+    });
+
+    // add cards to the record div
+
+    element.appendChild(profile);
+    element.appendChild(repos);
+
     return element;
 }
 
