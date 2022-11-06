2b33a72048a374588542f08a3a7cd0fcd1eaf101
Igor Alekhnovych <igoraleh@gmail.com>
2022-11-06 12:46:36 +0000

    Problem 3: Create Form

--- problem-3/script.js
+++ problem-3/script.js
@@ -1,1 +1,29 @@
-const mainElement = documet.querySelector('.main');
+const mainElement = document.querySelector('.main');
+
+// create form, input and search button
+
+const formElement = document.createElement('form');
+formElement.classList.add('search');
+formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const inputValue = Object.fromEntries(new FormData(e.target));
+});
+
+const inputElement = document.createElement('input');
+inputElement.classList.add('search-input');
+inputElement.setAttribute('name', 'name');
+
+const searchButtonElement = document.createElement('button');
+searchButtonElement.classList.add('search-button');
+searchButtonElement.setAttribute('type', 'submit');
+searchButtonElement.innerHTML = "Search";
+
+// add created elements to the form
+
+formElement.appendChild(inputElement);
+formElement.appendChild(searchButtonElement);
+
+// add form to the website
+
+mainElement.appendChild(formElement);
